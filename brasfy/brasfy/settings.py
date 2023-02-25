9c92bbe8a6ed7dff1545b991b408e33c5fd52224
MennoBR <malburgcine@hotmail.com>
2023-02-24 22:33:14 -0300

    fritobrasf1st

--- brasfy/brasfy/settings.py
+++ brasfy/brasfy/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+'''AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -90,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-]
+]'''
 
 
 # Internationalization
@@ -109,7 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '../static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- brasfy/brasfy/views.py
+++ brasfy/brasfy/views.py
@@ -1,9 +1,7 @@
+#definindo as views html:
 
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'brasfy/home.html')
-
-
-
-
